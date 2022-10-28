de0c38e4b7cd3bf05dc68300e03367f1f4034ef4
ruasvmv <maksim.suvorov@raiffeisen.ru>
2022-10-28 11:27:51 +0300

    fix inference predict

--- src/model/model.py
+++ src/model/model.py
@@ -11,7 +11,7 @@ class ToxicSegmenter(nn.Module):
             hidden_size: int,
             output_dim: int,
             dropout=0.5,
-            is_bidirectional: bool = False,
+            is_bidirectional: bool = True,
     ):
         super().__init__()
 
@@ -21,9 +21,17 @@ class ToxicSegmenter(nn.Module):
                             batch_first=True,
                             bidirectional=is_bidirectional)
 
-        self.fc = nn.Linear(2 * hidden_size, output_dim)
+        self.gru = nn.GRU(input_size=embedding_dim,
+                          hidden_size=hidden_size,
+                          num_layers=1,
+                          batch_first=True,
+                          bidirectional=is_bidirectional
+                          )
+
+        self.fc = nn.Linear((int(is_bidirectional) + 1) * hidden_size, output_dim)
 
     def forward(self, text):
-        x, (_, _) = self.lstm(text)
-
+        # x, (_, _) = self.lstm(text)
+        # return self.fc(x)
+        x, hidden = self.gru(text)
         return self.fc(x)
